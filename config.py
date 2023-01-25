804c4a184dd5c3d11bd8fe188d1a284f328b2e9a
Ozhegov1 <121109544+Ozhegov1@users.noreply.github.com>
2023-01-25 21:16:51 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "5951348166:AAHc9Ve1pl_6oYvfGnmdq96tGucV64mm18o"
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ main.py
@@ -0,0 +1,50 @@
+import telebot
+from extensions import APIException, Convertor
+from config import TOKEN, exchanges
+import traceback
+
+
+
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+
+}
+TOKEN = "5951348166:AAHc9Ve1pl_6oYvfGnmdq96tGucV64mm18o"
+
+bot = telebot.TeleBot (TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot):
+    text = "Приветствие!"
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    values = message.text.split(' ')
+    try:
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров!')
+
+        answer = Convertor.get_price(*values)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка в команде:\n{e}")
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        bot.reply_to(message, f"Неизвестная ошибка:\n{e}")
+    else:
+        bot.reply_to(message, answer)
+
+
+bot.polling()
